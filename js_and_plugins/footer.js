eef208808a678b1a2277fae137e13fc84d6db275
Bappy Golder <bups.co@gmail.com>
2016-11-25 15:50:12 +1100

    WIP for placing all links in one place

--- js_and_plugins/footer.js
+++ js_and_plugins/footer.js
@@ -1,3 +1,4 @@
 /*This file contains all the JavaScript file links & small JavaScript 
 that are included at the footer of our index file.*/
 
+//alert("hi");
--- js_and_plugins/main.js
+++ js_and_plugins/main.js
@@ -5,6 +5,5 @@ $("div.footer").append('<script src="js_and_plugins/footer.js"></script>');
 //animate page when loading
 $(".animsition").animsition();
 
-
 //show tooltip with help options
 //hide and show the button on input focus
