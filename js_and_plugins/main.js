c229f915a6ef2adea8fc4e9a763dd630152736aa
Bappy Golder <bups.co@gmail.com>
2016-12-03 20:00:29 +1100

    test file added
    for testing with JS objects or Json

--- js_and_plugins/main.js
+++ js_and_plugins/main.js
@@ -5,5 +5,17 @@ $("div.footer").append('<script src="js_and_plugins/footer.js"></script>');
 //animate page when loading
 $(".animsition").animsition();
 
+//quotes oject
+var all_quotes = {
+	q1 : {
+		"quote" : "Your past doesn't equal your future.",
+		"author" : "Tony Robbins"
+	}
+};
+
+//add quotes to page
+$("#qute_view").html(all_quotes.q1.quote);
+$("#author_view").append(all_quotes.q1.author);
+
 //show tooltip with help options
 //hide and show the button on input focus
