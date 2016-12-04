6c158dff5dea68343eb53c8123b29db086f7b58f
Bappy Golder <bups.co@gmail.com>
2016-12-04 21:52:53 +1100

    Object literral introduced

--- js_and_plugins/main.js
+++ js_and_plugins/main.js
@@ -5,16 +5,25 @@ $("div.footer").append('<script src="js_and_plugins/footer.js"></script>');
 //animate page when loading
 $(".animsition").animsition();
 
-//quotes oject
-var all_quotes = {
+//quotes oject stored using object literal
+/*var all_quotes = {
 	q1 : {
 		"quote" : "Your past doesn't equal your future.",
 		"author" : "Tony Robbins"
 	}
-};
+};*/
+
+//crate quote object using constructor function
+function All_quotes(quote, author){
+	"use strict";	
+	this.quote = quote;
+	this.author = author;	
+}
+
+var q1 = new All_quotes("Your past doesn't equal your future.", "Tony Robbins");
 
 //add quotes to page
-$("#qute_view").html(all_quotes.q1.quote);
+$("#qute_view").html(All_quotes.q1);
 $("#author_view").append(all_quotes.q1.author);
 
 //show tooltip with help options
