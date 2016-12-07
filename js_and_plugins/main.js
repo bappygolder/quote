dc6c3710ced868deaf1f68ec4fe023ba81982453
Bappy Golder <bups.co@gmail.com>
2016-12-07 16:58:05 +1100

    object update

--- js_and_plugins/main.js
+++ js_and_plugins/main.js
@@ -22,25 +22,25 @@ function All_quotes(quote, author){
 
 //all quotes instances - template: var qN = new All_quotes("", "");
 var q1 = new All_quotes(
-	"Your past doesn't equal your future.", 
-	"Tony Robbins"
-);
+			"Your past doesn't equal your future.", 
+			"Tony Robbins"
+		);
 var q2 = new All_quotes(
-	"The secret of change is to focus all your energy, not fighting the old, but on building the new.", 
-	"Socrates"
-);
+			"The secret of change is to focus all your energy, not fighting the old, but on building the new.", 
+			"Socrates"
+		);
 var q3 = new All_quotes(
-	"Keep calm and carry on.", 
-	"Winston Churchill"
-);
+			"Keep calm and carry on.", 
+			"Winston Churchill"
+		);
 var q4 = new All_quotes(
-	"Passion is like fire in your belly. If you contain it, it produces great results like light from fire. If you can't control it can consume you like devastating bushfire.", 
-	"Bappy Golder"
-);
+			"Passion is like fire in your belly. If you contain it, it produces great results like light from fire. If you can't control it can consume you like devastating bushfire.", 
+			"Bappy Golder"
+		);
 var q5 = new All_quotes(
-	"When everything seems to be going against you, remember that the airplane takes off against the wind, not with it.", 
-	"Henry Ford"
-);
+			"When everything seems to be going against you, remember that the airplane takes off against the wind, not with it.", 
+			"Henry Ford"
+		);
 
 //add quotes to page
 $("#qute_view").html(q1.quote);
