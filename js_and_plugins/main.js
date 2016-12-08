730100ee63c352027dc6ca88abbf33ec9291e765
Bappy Golder <bups.co@gmail.com>
2016-12-08 15:16:46 +1100

    variable updated

--- js_and_plugins/main.js
+++ js_and_plugins/main.js
@@ -6,7 +6,7 @@ $("div.footer").append('<script src="js_and_plugins/footer.js"></script>');
 $(".animsition").animsition();
 
 //quotes oject stored using object literal
-/*var all_quotes = {
+/*let all_quotes = {
 	q1 : {
 		"quote" : "Your past doesn't equal your future.",
 		"author" : "Tony Robbins"
@@ -20,24 +20,24 @@ function All_quotes(quote, author){
 	this.author = author;	
 }
 
-//all quotes instances - template: var qN = new All_quotes("", "");
-var q1 = new All_quotes(
+//all quotes instances - template: const qN = new All_quotes("", "");
+const q1 = new All_quotes(
 			"Your past doesn't equal your future.", 
 			"Tony Robbins"
 		);
-var q2 = new All_quotes(
+const q2 = new All_quotes(
 			"The secret of change is to focus all your energy, not fighting the old, but on building the new.", 
 			"Socrates"
 		);
-var q3 = new All_quotes(
+const q3 = new All_quotes(
 			"Keep calm and carry on.", 
 			"Winston Churchill"
 		);
-var q4 = new All_quotes(
+const q4 = new All_quotes(
 			"Passion is like fire in your belly. If you contain it, it produces great results like light from fire. If you can't control it can consume you like devastating bushfire.", 
 			"Bappy Golder"
 		);
-var q5 = new All_quotes(
+const q5 = new All_quotes(
 			"When everything seems to be going against you, remember that the airplane takes off against the wind, not with it.", 
 			"Henry Ford"
 		);
